1554bd5f10cb99fa936b78f23b1faa873cd08c65
Firuz Salman <salmanzade.firuz@gmail.com>
2022-04-09 11:46:34 +0400

    Admin panel: Ekle Sil

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -7,16 +7,32 @@ using Murat_Yucedag_Mvc5_TatilSeyahat_Sitesi.Models.Siniflar;
 
 namespace Murat_Yucedag_Mvc5_TatilSeyahat_Sitesi.Controllers
 {
-    public class AdminController : Controller
-    {
+     public class AdminController : Controller
+     {
           // GET: Admin
           Context ctx = new Context();
-        public ActionResult Index()
+          public ActionResult Index()
+          {
+               return View(ctx.Blogs.ToList());
+          }
+          public ActionResult YeniBlog()
+          {
+               Blog blog = new Blog();
+               return View(blog);
+          }
+          [HttpPost]
+        public ActionResult YeniBlog(Blog blog)
         {
-
-
-
-            return View(ctx.Blogs.ToList());
+               ctx.Blogs.Add(blog);
+               ctx.SaveChanges();
+             return RedirectToAction("Index");
+        }
+        public ActionResult BlogSil(int id)
+        {
+               var silinenBlog = ctx.Blogs.Find(id);
+               ctx.Blogs.Remove(silinenBlog);
+               ctx.SaveChanges();
+             return RedirectToAction("Index");
         }
     }
 }
