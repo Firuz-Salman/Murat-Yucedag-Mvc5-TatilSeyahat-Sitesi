50485348b24f8546eff4bf1022f3402c0a7d8edd
Firuz Salman <salmanzade.firuz@gmail.com>
2022-04-12 21:44:47 +0400

    Blogs Write new comment

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Murat_Yucedag_Mvc5_TatilSeyahat_Sitesi
                routes.MapRoute(
                    name: "Default",
                    url: "{controller}/{action}/{id}",
-                   defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
+                   defaults: new { controller = "Blog", action = "BlogDetay", id = 1/*UrlParameter.Optional*/ }
                );
           }
      }
