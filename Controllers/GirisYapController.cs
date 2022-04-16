332cc1a01e7830b99f66d9fb66d4e6e0deb1e2c3
Firuz Salman <salmanzade.firuz@gmail.com>
2022-04-17 00:39:36 +0400

    Login page added

---
+++ Controllers/GirisYapController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Murat_Yucedag_Mvc5_TatilSeyahat_Sitesi.Controllers
+{
+    public class GirisYapController : Controller
+    {
+        // GET: GirisYap
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
